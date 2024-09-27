d8221ac819cce1aaf22fd9a3333e9e2ca6100206
ntritin62 <21522677@gm.uit.edu.vn>
2024-09-27 10:25:51 +0700

    User - Addresses

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,70 @@
+class AddressesController < ApplicationController
+  before_action :set_user, :logged_in_user, :correct_user
+  before_action :set_address, only: %i(edit update destroy)
+
+  def index
+    @default_address = @user.addresses.default_address
+    addresses = @user.addresses.not_default_address.ordered_by_updated_at
+    @pagy, @addresses = pagy addresses, limit: Settings.ui.address_limit
+  end
+
+  def new
+    @address = @user.addresses.build
+  end
+
+  def create
+    Address.set_default_false(@user) if params.dig(:address, :default) == "1"
+    @address = @user.addresses.build(address_params)
+
+    if @address.save
+      flash[:success] = t ".success"
+      redirect_to user_addresses_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if params.dig(:address, :default) == "1" && @address.changed?
+      Address.set_default_false(@user)
+    end
+    if @address.update address_params
+      flash[:success] = t ".success"
+      redirect_to user_addresses_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @address.destroy
+      flash[:success] = t ".success"
+    else
+      flash[:danger] = t ".fail"
+    end
+    redirect_to user_addresses_path(@user)
+  end
+
+  private
+  def set_user
+    @user = User.find_by id: params[:user_id]
+    return if @user
+
+    flash[:warning] = t ".not_found"
+    redirect_to root_path
+  end
+
+  def set_address
+    @address = @user.addresses.find_by id: params[:id]
+    return if @address
+
+    flash[:warning] = t ".not_found_address"
+    redirect_to user_addresses_path(@user)
+  end
+
+  def address_params
+    params.require(:address).permit Address::ADDRESS_REQUIRE_ATTRIBUTES
+  end
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,20 @@
 class Address < ApplicationRecord
+  ADDRESS_REQUIRE_ATTRIBUTES = %i(receiver_name place phone default).freeze
   belongs_to :user
 
   validates :receiver_name, presence: true,
             length: {maximum: Settings.value.max_receiver_name}
   validates :place, presence: true,
             length: {maximum: Settings.value.max_place_length}
-  validates :phone, presence: true, length: {is: Settings.value.phone},
-            format: {with: Settings.value.phone_format}
+  validates :phone, presence: true, format: {with: Settings.value.phone_format}
+
+  scope :ordered_by_updated_at, ->{order(updated_at: :desc)}
+  scope :default_address, ->{where(default: true)}
+  scope :not_default_address, ->{where(default: false)}
+
+  class << self
+    def set_default_false user
+      user.addresses.update_all(default: false)
+    end
+  end
 end
