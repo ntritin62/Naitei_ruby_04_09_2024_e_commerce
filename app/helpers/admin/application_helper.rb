cc37d17cca0b2ffec29b6e1613150de289b177ad
Hongtien2910 <hongtien003@gmail.com>
2024-10-15 23:28:08 +0700

    image

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,7 +1,7 @@
 # app/helpers/admin/application_helper.rb
 module Admin::ApplicationHelper
   def product_image product
-    product.img_url.presence || "default_image_url_here"
+    product.image.presence || "default_image_url_here"
   end
 
   def active_class link_path
--- db/migrate/20241001094437_add_img_url_to_products.rb
+++ db/migrate/20241001094437_add_img_url_to_products.rb
@@ -1,5 +1,5 @@
 class AddImgUrlToProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :products, :img_url, :string
+    add_column :products, :image, :string
   end
 end
